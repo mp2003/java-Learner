67e88235a28b9b42c95fdcd8b8c42430a9c75b6a
mp2003 <m.p6092003@gmail.com>
2024-10-08 23:06:38 +0530

    pattern.java

---
+++ AgeSalaryChecker.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AgeSalaryChecker {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+
+        int age = sc.nextInt();
+        int salary = sc.nextInt();
+        
+        if (age > 40) {
+            if (salary >= 30000) {
+                System.out.println("You are rich and adult");
+            } else {
+                System.out.println("You are an adult");
+            }
+        } else {
+            if (salary >= 12000) {
+                System.out.println("You are rich and young");
+            } else {
+                System.out.println("You are young");
+            }
+        }
+
+    }
+}
--- Oldest.java
+++ Oldest.java
@@ -1,3 +1,21 @@
+
+import java.util.Scanner;
+
 public class Oldest {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a > b && a > c) {
+            System.out.println("A");
+        } else if (a < b && b > c) {
+            System.out.println("B");
+        } else {
+            System.out.println("C");
+        }
+    }
 }
