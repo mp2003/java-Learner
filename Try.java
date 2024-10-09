04db1dd1dc3453bca0e8c6fa650c3d7b6fe06de9
mp2003 <m.p6092003@gmail.com>
2024-10-09 23:42:28 +0530

    Exercise 1 answer

---
+++ Try.java
@@ -0,0 +1,8 @@
+
+public class Try {
+
+    public static void main(String[] args) {
+        char a = 'a';
+        System.out.println(a++);
+    }
+}
---
+++ newFile2.java
@@ -0,0 +1,7 @@
+
+public class newFile2 {
+    public static void main(String[] args) {
+        char a = 'a';
+        System.out.println(++a);
+    }
+}
