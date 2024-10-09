9f6ee8e2e9d79f8ff416b2c9fcb507af2b68a71a
mp2003 <m.p6092003@gmail.com>
2024-10-09 23:15:07 +0530

    Exercise 1 answer

---
+++ newFile.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class newFile {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+
+        System.out.println((int)Math.pow(x, n));
+    }
+}
