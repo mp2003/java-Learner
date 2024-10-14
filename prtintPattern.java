e9d9a44a54c1682ebba7dc4349d2b54ed6ba0113
mp2003 <m.p6092003@gmail.com>
2024-10-14 21:25:23 +0530

    Exercise 1 answer

--- prtintPattern.java
+++ prtintPattern.java
@@ -6,7 +6,7 @@ public class prtintPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = 9;
-        int q = 20;
+        int q = sc.nextInt();
         int num = 1;
         while (num <= q) {
             System.out.print(n * num + " ");
