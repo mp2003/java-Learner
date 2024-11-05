ad2ff02937419b018244b0cbf207cef0cfb772b9
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-11-05 20:28:11 +0530

    Update CRUDarray.java

--- CRUDarray.java
+++ CRUDarray.java
@@ -6,8 +6,9 @@ public class CRUDarray {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[5];
-        for (int i = 0; i < 3; i++) {
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
